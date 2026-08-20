54d9c658e4d1316a4300365d2ba6150f7c6444a7
zythros <zythros@users.noreply.github.com>
2026-08-20 15:12:54 -0700

    Add trailing space after %p in datetime format to nudge bar text left

--- config.h
+++ config.h
@@ -71,5 +71,5 @@ static const struct arg args[] = {
     // { battery_state,  "^c#f43f5e^%s^d^ ",           "BAT0" },
     // { battery_perc,   "^c#f43f5e^ %s%%^d^",         "BAT1" },
     // { battery_state,  "^c#f43f5e^%s^d^ ",           "BAT1" },
-    { datetime,       "^b#222222^^c#eeeeee^󰸗 %s^d^",   "%a %b %d  %I:%M %p" },
+    { datetime,       "^b#222222^^c#eeeeee^󰸗 %s^d^",   "%a %b %d  %I:%M %p " },
 };
